fcfa412544261753de4c2af83683a0388c58a5d7
sojin9911 <soj2204@gmail.com>
2022-02-03 01:18:07 +0900

    제발 끝인걸로 해주세요 ㅠㅠㅠㅠㅠ

---
+++ script.js
@@ -0,0 +1,51 @@
+let mobileMenu = document.querySelectorAll('.mobile-menu');
+let tabletMenu = document.querySelectorAll('.tablet-menu');
+let mobileIndex =0;
+let tabletIndex = 0;
+    
+for (let i=0; i < mobileMenu.length; i++) {
+    mobileMenu[i].addEventListener('click', ()=> {
+        mobileMenu[mobileIndex].classList.remove('active');
+        mobileMenu[i].classList.add('active');
+        mobileIndex = i;
+    });
+
+    mobileMenu[i].addEventListener('mouseout', ()=> {
+        mobileMenu[i].classList.remove('active');
+    })
+
+    tabletMenu[i].addEventListener('click', ()=> {
+        console.log(i,"누름")
+        tabletMenu[tabletIndex].classList.remove('active');
+        tabletMenu[i].classList.add('active');
+        tabletIndex = i;
+    });
+    tabletMenu[i].addEventListener('mouseout', ()=> {
+        tabletMenu[i].classList.remove('active');
+    })
+
+}
+
+
+const sun = document.querySelector('.sunClock');
+let start = new Date("2021-12-31 07:34:00");
+let end = new Date("2021-12-31 17:17:00");
+let stT =start.getHours()*60 + start.getMinutes();
+let enT =end.getHours()*60 + end.getMinutes();
+
+let startAngle = stT/4;
+let endAngle = enT/4;
+
+sun.style.backgroundImage="conic-gradient(#11264f " + startAngle + "deg, #ff7473 0 " + endAngle + "deg, #11264f 0)";
+
+
+const moon = document.querySelector('.moonClock');
+let mori = new Date("2021-12-31 00:59:00");
+let mose = new Date("2021-12-31 13:01:00");
+let moriT =mori.getHours()*60 + mori.getMinutes();
+let moseT =mose.getHours()*60 + mose.getMinutes();
+
+let moriAngle = moriT/4;
+let moseAngle = moseT/4;
+
+moon.style.backgroundImage="conic-gradient(#11264f " + moriT + "deg, #f0d047 0 " + moseAngle + "deg, #11264f 0)";
